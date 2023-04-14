6af7984e33618b31f6d2f234fa7d61d3a425af6c
Jorge Netto <jorgenetto96@gmail.com>
2023-04-14 04:55:51 -0300

    Exercise 02, test #1 - passed !

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    //input: string, num
+    // stringStringString
+    let result = "";
+    for(let i = 0; i < num; i++) {
+        result = result + string;
+    }
+    return result;
 };
 
+
+// make a function that returns a string "hey" 3 times. repeatString("hey", 3)
 // Do not edit below this line
 module.exports = repeatString;
