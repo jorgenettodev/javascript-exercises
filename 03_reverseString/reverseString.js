a688b070e20d315d5e1dd52d246f483599972b55
Jorge Netto <jorgenetto96@gmail.com>
2023-04-14 06:02:21 -0300

    Exercise 03 - Write a function that reverse a string. All tests passed.
    Step # 1 - Turn the string into an array.
    Step # 2 - Reverse the array using Array.reverse()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    arr.reverse();
+    let newString = arr.join("");
+    return newString;
 };
+
+// write a function that: take a string as input and return the same string, but reversed, 
+// OUTPUT: for instance: "ABC" == "CBA"
+// Processing: Take the string and turn it into an array.
+// then take firstArray = ["A","B","C"] and write a secondArray, passing always the LAST ELEMENT of the first array.
+// secondArray = firstArray[lastElement].push();
+// 
+// When there is no more elements, then .join(",") the secondArray into a string
+// return the string.
 
 // Do not edit below this line
 module.exports = reverseString;
