9691b94c8d8c4a06b71ee51c33f6a7ca03986b9e
Jorge Netto <jorgenetto96@gmail.com>
2023-04-14 15:58:02 -0300

    Exercise 04 - done Done some googling to look up for up for 'rest parameters'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    array.forEach(element => {
+        for (const arg of args) {
+            if (array.includes(arg)) {
+                array.splice(array.indexOf(arg),1);
+            }
+            continue;
+        }
+    });
 
+    return array;
 };
+
+console.log(removeFromArray([1,2,3,4,"netto"],"netto",3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
