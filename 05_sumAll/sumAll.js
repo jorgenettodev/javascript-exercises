c842eef35de904945e23c07cf82145953bae5740
Jorge Netto <jorgenetto96@gmail.com>
2023-04-16 18:35:43 -0300

    Exercise 05 - done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result = 0;
+
+    
+
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a == null || b == null) {
+        return "ERROR";
+    }
+
+    
+    
+    
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+
+    } else if (a > b) {
+        for (let i = b; i <= a; i++) {
+            result += i ;
+        }
+        
+    }
+    if (typeof a !== "number" || typeof b !== "number" ) {
+        return "ERROR";
+    }
+    
+    return result;
 
 };
 
