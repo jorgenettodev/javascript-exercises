7e78423e19d716809a5357a2c758fbbf0a815ca2
Jorge Netto <jorgenetto96@gmail.com>
2023-05-31 05:15:51 -0300

    calculator and palindrome = done.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  
+  let result = array.reduce((total, element) => total + element, 0);
+  return result;
+	};
+
+const multiply = function(array) {
+  let result = array.reduce((total, element) => {
+    return total * element;
+  }, 1);
+  return result;
 };
 
-const sum = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const multiply = function() {
+const factorial = function(n) {
+	if (n == 1) {
+    return 1;
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  if (n == 0) {
+    return 1;
+  }
+  return n * factorial(n -1);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    string = string.replace(/[^a-zA-Z]/g, '');
+    
+
+    const reverseString = [...string].reverse().join("");
+    if (string.toLowerCase() == reverseString.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
