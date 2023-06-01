588e1c617977bf3994c732cadac3512a9530f72b
Jorge Netto <jorgenetto96@gmail.com>
2023-06-01 05:10:25 -0300

    fibonacci series.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    if (n <= 1) {
+        return n;
+    }
+
+    let n1 = 0;
+    let n2 = 1;
+    
+
+    for (let i = 2; i <= n; i++){
+        let nextNumber = n2 + n1;
+        n1 = n2;
+        n2 = nextNumber;
+    }
+    return n2;
 
 };
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(5));
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
